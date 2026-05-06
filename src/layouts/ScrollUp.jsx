9fa5706518aed66ce8e84bdb1de64a0db1482681
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-05-06 19:43:55 +0300

    updated

--- src/layouts/ScrollUp.jsx
+++ src/layouts/ScrollUp.jsx
@@ -1,7 +1,7 @@
 import { ArrowUpIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
-export default function ScrollUP({isScroll}){
+export default function ScrollUP(){
     const scrollTop = () => window.scrollTo({
         top: 0,
         behavior: "smooth"
@@ -12,7 +12,7 @@ export default function ScrollUP({isScroll}){
             <Button 
                 variant="outline" 
                 size="icon" 
-                className={`bg-indigo-500 hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-700 ${isScroll ? "flex" : "hidden transition duration-700 ease-in-out"}`} 
+                className="bg-indigo-500 transition duration-700 ease-in-out hover:bg-indigo-700 dark:bg-indigo-500 dark:hover:bg-indigo-700" 
                 onClick={scrollTop}
             >
                 <ArrowUpIcon className="text-white"/>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,7 +11,7 @@ import Navbar from "@/layouts/Navbar";
 export default function Landing(){
     const [isScroll, setIsScroll] = useState(false)
     useEffect(() =>{
-        const handleScroll = () => window.scrollY > 50 ? setIsScroll(true) : setIsScroll(false);
+        const handleScroll = () => window.scrollY > 90 ? setIsScroll(true) : setIsScroll(false);
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     },[])
@@ -19,7 +19,7 @@ export default function Landing(){
     return(
         <div className="relative bg-muted/50">
             <Navbar/>
-            <ScrollUP scrollUP={isScroll}/>
+            {isScroll && <ScrollUP/>}
             <SectionAbout/>
             <SectionSkills/>
             <SectionProjects/>
